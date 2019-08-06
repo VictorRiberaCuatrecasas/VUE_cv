9d9f2c99290a60606dd380fe0a54aa98e43f3361
Victor <victorribera92@gmail.com>
2019-08-06 15:48:25 +0200

    footers

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,7 +4,28 @@
       <p class="hTitle">Contact</p>
     </header>
     <v-img class="mirrorIMG" src="../assets/blueb.png"></v-img>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nemo aliquid nihil magnam consequuntur pariatur adipisci soluta odit cum aliquam quasi doloremque asperiores reprehenderit reiciendis distinctio ab voluptatem, corporis id inventore facilis laudantium excepturi odio repellat fuga. Fugit asperiores voluptatum ex laudantium, soluta voluptas molestias architecto, magnam, quam ea cupiditate commodi suscipit accusantium? Libero enim quae dolorem dolor optio quas officiis quod natus accusantium cum velit quos debitis placeat voluptatum repudiandae labore, itaque facere explicabo quasi rerum consectetur eligendi magni dolore totam. Impedit consequatur, aliquid non vel, nihil rerum sapiente iusto, ducimus molestiae facilis nisi atque placeat culpa corporis dicta reprehenderit sint reiciendis. Numquam nam cum quasi officiis placeat id exercitationem minima possimus sint a inventore facere hic impedit illo veritatis unde quis voluptatum sed, voluptates perferendis esse molestiae odio labore modi. Earum nam molestias, ad adipisci eum quod aut reprehenderit voluptate dolorem voluptates consequatur perferendis quo sapiente sit ea!</p>
+
+    <v-img src="../assets/footer3.png"></v-img>
+    <footer class="footer3">
+      <div>
+        <a target="_blank" href="https://github.com/VictorRiberaCuatrecasas">
+          <img src="../assets/github.png" alt />
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/victor-r-c/">
+          <img src="../assets/linkedin.png" alt />
+        </a>
+      </div>
+      <div>
+        <router-link class="footerNav" to="/">About</router-link>
+        <router-link class="footerNav" to="/Work">Work</router-link>
+        <router-link class="footerNav" to="/Contact">Contact</router-link>
+      </div>
+      <hr />
+      <p class="footerBottom">
+        &copy; {{ new Date().getFullYear() }} |
+        <strong>Victor Ribera</strong>
+      </p>
+    </footer>
   </div>
 </template>
 
@@ -13,6 +34,55 @@ export default {};
 </script>
 
 <style>
+.footer3 {
+  background-color: #00003b;
+  color: white;
+}
+.footer2 {
+  background-color: #031d22;
+  color: white;
+}
+.footer1 {
+  background-color: #210002;
+  color: white;
+}
+.footerBottom {
+  text-align: center;
+  padding: 16px 0 16px 0;
+  margin: 0;
+}
+.v-application p {
+  margin-bottom: 0 !important;
+}
+footer div a img {
+  max-height: 25px;
+  max-width: 25px;
+  margin: 16px 2em 10px;
+}
+footer div a img:hover {
+  padding-bottom: 6px;
+}
+.footerNav {
+  margin: 0px 2.5% 032px;
+  text-decoration: none;
+  color: white !important;
+}
+.footerNav:hover {
+  font-weight: bold;
+}
+hr {
+  border: none;
+  height: 2px;
+  /* Set the hr color */
+  color: white; /* old IE */
+  background-color: white; /* Modern Browsers */
+}
+
+footer div {
+  display: flex;
+  justify-content: center;
+}
+
 .header3 {
   background-image: url("../assets/blue.png");
   height: 100vh;
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -116,6 +116,28 @@
       </div>
       <v-img src="../assets/worklast.png"></v-img>
     </div>
+
+    <v-img src="../assets/footer2.png"></v-img>
+    <footer class="footer2">
+      <div>
+        <a target="_blank" href="https://github.com/VictorRiberaCuatrecasas">
+          <img src="../assets/github.png" alt />
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/victor-r-c/">
+          <img src="../assets/linkedin.png" alt />
+        </a>
+      </div>
+      <div>
+        <router-link class="footerNav" to="/">About</router-link>
+        <router-link class="footerNav" to="/Work">Work</router-link>
+        <router-link class="footerNav" to="/Contact">Contact</router-link>
+      </div>
+      <hr />
+      <p class="footerBottom">
+        &copy; {{ new Date().getFullYear() }} |
+        <strong>Victor Ribera</strong>
+      </p>
+    </footer>
   </div>
 </template>
 
