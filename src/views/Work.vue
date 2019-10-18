fa7488d4799b86a32a9b3cea50a997a3d4976c02
Victor <victorribera92@gmail.com>
2019-10-18 22:18:39 +0200

    added work

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -24,6 +24,7 @@
         >Here's a selection of some recent work. I've been dedicating most of my time on trying things and learning new languages.</p>
       </div>
 
+      <!-- TGIF TRANSPARENCY -->
       <v-img src="../assets/work1.png"></v-img>
       <div class="projects project1">
         <img
@@ -52,26 +53,27 @@
         </div>
       </div>
 
+      <!-- NEWSLETTER -->
       <v-img src="../assets/work2.png"></v-img>
       <div class="projects project2">
         <img
           data-aos="zoom-in"
           data-aos-duration="2000"
-          class="projectIMG"
-          src="../assets/tindogproject.png"
+          class="projectIMG2"
+          src="../assets/newsletterfram1.png"
         />
       </div>
       <div class="projectDescription2 no-overflow">
         <div data-aos="fade-left" data-aos-duration="2000">
-          <h2>Tindog</h2>
+          <h2>VRC Newsletter</h2>
           <p>
-            Very basic site made only to practise responsive design with bootstrap grid and classes.
+            Basic newsletter subscription connected to Mailchimp as my introduction to back-end with node.
             <br />
-            <br />♢BOOTSTRAP ♢NETLIFY
+            <br />♢JS ♢BOOTSTRAP ♢NODE ♢NODE ♢EXPRESS
           </p>
           <v-btn
             target="_blank"
-            href="https://vtindog.netlify.com/"
+            href="https://vrc-newsletter.herokuapp.com/"
             class="projectBTN"
             color="#9bb36f"
           >
@@ -80,6 +82,7 @@
         </div>
       </div>
 
+      <!-- SIMON GAME-->
       <v-img src="../assets/work3.png"></v-img>
       <div class="projects project3">
         <img
@@ -103,6 +106,7 @@
         </div>
       </div>
 
+      <!-- MG TEAM -->
       <v-img src="../assets/work4.png"></v-img>
       <div class="projects project4">
         <img
@@ -130,6 +134,61 @@
           </v-btn>
         </div>
       </div>
+
+      <!-- NODE BLOG-->
+      <v-img src="../assets/work3.png"></v-img>
+      <div class="projects project3">
+        <img
+          data-aos="zoom-in"
+          data-aos-duration="2000"
+          class="projectIMG"
+          src="../assets/blogproject.png"
+        />
+      </div>
+      <div class="projectDescription1">
+        <div data-aos="fade-right" data-aos-duration="2000">
+          <h2>VRC Blog</h2>
+          <p>
+            Blog where you can create single pages dynamicaly for each posts submited in a hidden page (/compose).
+            <br />
+            <br />♢JS ♢EJS ♢NODE ♢EXPRESS ♢MONGOOSE ♢MONGODB
+          </p>
+          <v-btn target="_blank" href="https://vrc-blog.herokuapp.com/" class="projectBTN">
+            <span style="color:#9bb36f">View Site</span>
+          </v-btn>
+        </div>
+      </div>
+
+      <!-- TO DO LIST -->
+      <v-img src="../assets/work4.png"></v-img>
+      <div class="projects project4">
+        <img
+          data-aos="zoom-in"
+          data-aos-duration="2000"
+          class="projectIMG2"
+          src="../assets/todolistframe1.png"
+        />
+      </div>
+      <div class="projectDescription2 no-overflow">
+        <div data-aos="fade-left" data-aos-duration="2000">
+          <h2>VRC To-Do-List</h2>
+          <p>
+            A ToDO list, where you can add and remove stuff aswell as dynamicaly create other lists. Made in order to learn more about templating and partials.
+            <br />
+            <br />♢JS ♢EJS ♢BOOTSTRAP ♢NODE ♢EXPRESS ♢MONGOOSE ♢MONGODB
+          </p>
+          <v-btn
+            target="_blank"
+            href="https://vrc-todolist.herokuapp.com/"
+            class="projectBTN"
+            color="#9bb36f"
+          >
+            <span style="color:#031d22">View Site</span>
+          </v-btn>
+        </div>
+      </div>
+
+      <!-- ALLA QUE VOY -->
       <v-img src="../assets/work5.png"></v-img>
       <div class="projects project5">
         <img
@@ -238,6 +297,11 @@ export default {
   max-width: 50%;
   padding-top: 2em;
 }
+.projectIMG2 {
+  max-height: 600px;
+  max-width: 35%;
+  padding-top: 2em;
+}
 .pencil {
   width: 20%;
   max-width: 250px;
@@ -271,6 +335,11 @@ export default {
   .projectIMG {
     max-width: 90%;
     height: 250px;
+  }
+  .projectIMG2 {
+    max-width: 60%;
+    max-height: 300px;
+    padding-top: 2em;
   }
   .projectsText h2 {
     max-width: 90%;
