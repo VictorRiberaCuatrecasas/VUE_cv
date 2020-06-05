4f359a65cd8c0cf8ff2d5e7f0721100cf98ba9bd
Victor <victorribera92@gmail.com>
2020-06-05 14:47:19 +0200

    new eng cv

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: 'UA-168545292-1',
+  router
+});
 
 new Vue({
   router,
